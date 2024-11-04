78d90db2ba75f95e333d51234b30b659bba8b411
yousaf <yousafsomroo35@gmail.com>
2024-11-04 05:13:42 -0800

    first chat_bot by using chatbase

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
